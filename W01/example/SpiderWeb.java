7283b47a66122cb405fbed4ae6c3101d68c28dd1
Chun <caochun@gmail.com>
2021-09-10 13:53:31 +0800

    W01

---
+++ W01/example/SpiderWeb.java
@@ -0,0 +1,12 @@
+package W01.example;
+
+public class SpiderWeb extends Being {
+
+    public void open() {
+        System.out.println("I open. ");
+    };
+
+    public void close() {
+        System.out.println("I close. ");
+    };
+}
