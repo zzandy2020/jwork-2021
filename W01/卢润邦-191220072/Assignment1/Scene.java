b6d1ab149d99b65bb836798a2c4806c660c2dd7f
runbanglu <2320470008@qq.com>
2021-09-11 09:58:30 -0700

    coding part is done

--- W01/卢润邦-191220072/Assignment1/Scene.java
+++ W01/卢润邦-191220072/Assignment1/Scene.java
@@ -1,5 +1,49 @@
 package Assignment1;
 
+import java.util.ListIterator;
+
 public class Scene {
+    Scorpion scorpion;
+    Snake snake;
+    Bigbro bigbro;
+    Monster centipede;
+    Subtitle subtitle;
+    Scene(){
+        scorpion = Scorpion.getInstance();
+        snake = Snake.getInstance();
+        bigbro = Bigbro.getInstance();
+        centipede = new Monster(500, 10, 0, "centipede");
+        subtitle = new Subtitle("Assignment1/Dialogue.txt");
+    }
+
+    void play(){
+        ListIterator<String> line = subtitle.Iterator();
+        System.out.println("*******\nAction!\n*******");
+        bigbro.speak(line.next());
+        snake.pondering(2);
+        snake.speak(line.next());
+        bigbro.speak(line.next());
+        scorpion.speak(line.next());
+        bigbro.speak(line.next());
+        scorpion.speak(line.next());
+        scorpion.getarmed("arrow", 50);
+        scorpion.attack(bigbro);
+        scorpion.lostArm();
+        snake.magic(bigbro);
+        centipede.attack(bigbro);
+        centipede.attack(bigbro);
+        bigbro.attack(centipede);
+        centipede.transform();
+        bigbro.becomeMega();
+        bigbro.attack(centipede);
+        centipede.isDead();
+        bigbro.becomeNormal();
+        System.out.println("*******\n  End  \n*******");
+    }
+
+    public static void main(String args[]){
+        Scene p3 = new Scene();
+        p3.play();
+    }
     
 }
