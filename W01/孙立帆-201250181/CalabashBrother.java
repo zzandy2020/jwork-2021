841df21e011b1483d7326dde827f467d8e0e135d
SUNLIFAN <1656680420@qq.com>
2021-09-11 14:44:26 +0800

    w01

---
+++ W01/孙立帆-201250181/CalabashBrother.java
@@ -0,0 +1,6 @@
+public class CalabashBrother extends Creature{
+public CalabashBrother(int health,int attack,String name){
+    super(health, attack, name);
+}
+
+}
---
+++ W01/孙立帆-201250181/Monster.java
@@ -0,0 +1,9 @@
+public class Monster extends Creature{
+    public Weapon weapon;
+    public Monster(int health,int attack,String name,Weapon weapon){
+        super(health,attack,name);
+        this.weapon = weapon;
+    }
+
+
+}
