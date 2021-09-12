e3fbedb58cebc0c37efa92f7bb510a54eef9ab05
runbanglu <2320470008@qq.com>
2021-09-12 02:29:09 -0700

    TODO README.MD

---
+++ W01/191220072/Assignment1/Subtitle.java
@@ -0,0 +1,32 @@
+package Assignment1;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+
+public class Subtitle{
+    List<String> contentList;
+    Subtitle(String DiaglougePath){
+        Path path = Paths.get(DiaglougePath);
+        try{
+            contentList = Files.readAllLines(path);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+            System.exit(-1);
+        } 
+    }
+
+    public ListIterator<String> Iterator(){
+        return contentList.listIterator();
+    }
+
+    public static void main(String args[]){
+        Subtitle s = new Subtitle("Assignment1/Dialogue.txt");
+        ListIterator<String> line = s.Iterator();
+        while(line.hasNext())
+            System.out.println(line.next());
+    }
+}
