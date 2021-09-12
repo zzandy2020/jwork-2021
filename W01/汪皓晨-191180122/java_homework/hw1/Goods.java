6e81072e67af3335b2c28fb14de4e1675bcb93da
淮水之余 <62435803+njuwhc@users.noreply.github.com>
2021-09-12 12:17:14 +0800

    Delete W01/汪皓晨-191180122/java_homework/hw1 directory

--- W01/汪皓晨-191180122/java_homework/hw1/Goods.java
+++
@@ -1,7 +0,0 @@
-package java_homework.hw1;
-
-public class Goods {
-    int temperature;
-    String name;
-    
-}
--- W01/汪皓晨-191180122/java_homework/hw1/Stone.java
+++
@@ -1,8 +0,0 @@
-package java_homework.hw1;
-
-public class Stone extends Goods {
-    Stone(){
-        temperature=25;
-    }
-    
-}
