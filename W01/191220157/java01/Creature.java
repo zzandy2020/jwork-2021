2a02f57d7704db794cd663b61dfece85c43913ab
张布仲 <cdd@zhangbuzhongdeMacBook-Pro.local>
2021-09-16 00:10:44 +0800

    java1

--- W01/191220157/java01/Creature.java
+++
@@ -1,8 +0,0 @@
-package java01;
-
-public class Creature {
-    String name;
-    public void speak(String word) {
-        System.out.println(name + "说：“" + word + "”");
-    }
-}
--- W01/191220157/java01/Ruyi.java
+++
@@ -1,8 +0,0 @@
-package java01;
-
-public class Ruyi {
-    public Wine prepare() {
-        System.out.println("【如意显灵ing】");
-        return new _Wine();
-    }
-}
